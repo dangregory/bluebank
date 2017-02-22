a3b1053c81f06ef8c3dc0aec495a0f1a4b0f3485
Daniel Gregory <dangregory@hotmail.com>
2017-02-22 18:23:12 -0300

    Test commit

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@
 
 // Declare app level module which depends on views, and components
 
-var app = angular.module('myApp',['ngRoute']);
+var app = angular.module('myApp',['ngRoute', 'ui.utils.masks']);
 
 
 app.config(function($routeProvider, $locationProvider)
@@ -31,5 +31,64 @@ app.config(function($routeProvider, $locationProvider)
       templateUrl : 'views/main.html',
       controller  : 'MainCtrl',
    })
+
+   .when('/transf', {
+      templateUrl : 'views/transf.html',
+      controller  : 'TransfCtrl',
+   })
  
 });
+
+app.factory('users', function($http){
+
+   var dados = {};
+   dados.dado = [];
+
+
+   $http({
+        method: 'GET',
+        url: 'http://private-915401-bluebank.apiary-mock.com/users'
+      }).then(function successCallback(response) {
+         for (var i = 0; i < response.data.length; i++) {
+            dados.dado.push(response.data[i]);
+         }
+         //logado = users.dados[0];
+        }, function errorCallback(response) {
+          // called asynchronously if an error occurs
+          // or server returns response with an error status.
+        });
+
+
+  return {
+      dados : dados.dado
+  };
+
+});
+
+app.factory('accounts', function($http){
+
+
+   var contas = {};
+   contas.dado = [];
+
+   $http({
+        method: 'GET',
+        url: 'http://private-915401-bluebank.apiary-mock.com/accountType'
+      }).then(function successCallback(response) {
+         for (var i = 0; i < response.data.length; i++) {
+            contas.dado.push(response.data[i]);
+         }
+         //logado = users.dados[0];
+        }, function errorCallback(response) {
+          // called asynchronously if an error occurs
+          // or server returns response with an error status.
+        });
+
+  return {
+      contas : contas.dado
+  };
+
+});
+
+
+
--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -1,7 +1,14 @@
-app.controller('LoginCtrl', function($rootScope, $location)
+app.controller('LoginCtrl', function($rootScope, $location, $scope, users)
 {
-	console.log("caiu2");
    $rootScope.activetab = $location.path();
+
+   $rootScope.dados = users.dados;
+
+   $rootScope.layout = [
+		   {type: 'header', path:'layout/header.html'}, 
+		   {type: 'footer', path:'layout/footer.html'}
+	   ];
+
 
    $( ".input" ).focusin(function() {
 	  $( this ).find( "span" ).animate({"opacity":"0"}, 200);
@@ -12,12 +19,34 @@ app.controller('LoginCtrl', function($rootScope, $location)
 	});
 
 	$(".login").submit(function(){
+
+		
+
+		$scope.findID = function(loginID) {
+		    for (var i = 0; i < $rootScope.dados.length; i++) {
+		        if ($rootScope.dados[i].id === loginID){
+		            $rootScope.selecionado = $rootScope.dados[i]; // Return as soon as the object is found
+		        	console.log($rootScope.selecionado);
+		        	$scope.submitQuery();
+		        }
+		    }
+		    return null; // The object was not found
+		}
+
+
+
 	  $(this).find(".submit i").removeAttr('class').addClass("fa fa-check").css({"color":"#fff"});
 	  $(".submit").css({"background":"#2ecc71", "border-color":"#2ecc71"});
 	  $(".feedback").show().animate({"opacity":"1", "bottom":"-80px"}, 400);
 	  $("input").css({"border-color":"#2ecc71"});
 	  return false;
 	})
+
+	$scope.submitQuery = function () {
+		$location.path('/main');
+	};
+
+
 });
  
 app.controller('ContactCtrl', function($rootScope, $location)
@@ -30,34 +59,162 @@ app.controller('AboutCtrl', function($rootScope, $location)
    $rootScope.activetab = $location.path();
 });
 
-app.controller('MainCtrl', function($rootScope, $location, $scope, $http)
+app.controller('MainCtrl', function($rootScope, $location, $scope, $http, users)
 {
-   $rootScope.activetab = $location.path();
+   $rootScope.activetab = $location.path();    
 
-   $scope.layout = [
-		   {type: 'header', path:'layout/header.html'}, 
-		   {type: 'footer', path:'layout/footer.html'}
-	   ];
+    $scope.hasChanged = function() {
+		$("#agenciaDestino").val("");
+		$("#contaDestino").val("");
+	};
 
-        $scope.dados = {};
-
-        $scope.selec = "123456";
-
-        $scope.getResult = function(dado){
-		    return dado.id == $scope.selec;
+	$scope.teste = function(dado, transValue){
+		if(transValue > dado.saldo){
+			$scope.erro = true;
 		};
+		console.log(dado);
+	};
 
-        $scope.dados = [
-        	{id:"123456", titular:"Daniel", saldo: -1000, limite: 5000, poupan: 20000},
-        	{id:"123457", titular:"Maria", saldo: 1000, limite: 5000, poupan: 1000},
-        	{id:"123458", titular:"José", saldo: 52000, limite: 8000, poupan: 4000},
-        	{id:"123459", titular:"Fernanda", saldo: 400, limite: 1000, poupan: 200},
-        	{id:"123450", titular:"Ana", saldo: 9000, limite: 20000, poupan: 10000}
-        ];
+	$scope.newTransfer = function(){
+    };
+});
+
+app.controller('TransfCtrl', function($rootScope, $location, $scope, $http, accounts)
+{
+   	$rootScope.activetab = $location.path();
+
+   	$rootScope.contas = accounts.contas;
+   	console.log($rootScope.contas);
+    	
+	var current_fs, next_fs, previous_fs; //fieldsets
+	var left, opacity, scale; //fieldset properties which we will animate
+	var animating; //flag to prevent quick multi-click glitches
+
+	$(".next").click(function(){
+		if(animating) return false;
+		animating = true;
+		
+		current_fs = $(this).parent();
+		next_fs = $(this).parent().next();
+		
+		//activate next step on progressbar using the index of next_fs
+		$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+		
+		//show the next fieldset
+		next_fs.show(); 
+		//hide the current fieldset with style
+		current_fs.animate({opacity: 0}, {
+			step: function(now, mx) {
+				//as the opacity of current_fs reduces to 0 - stored in "now"
+				//1. scale current_fs down to 80%
+				scale = 1 - (1 - now) * 0.2;
+				//2. bring next_fs from the right(50%)
+				left = (now * 50)+"%";
+				//3. increase opacity of next_fs to 1 as it moves in
+				opacity = 1 - now;
+				current_fs.css({
+	        'transform': 'scale('+scale+')',
+	        'position': 'absolute'
+	      });
+				next_fs.css({'left': left, 'opacity': opacity});
+			}, 
+			duration: 800, 
+			complete: function(){
+				current_fs.hide();
+				animating = false;
+			}, 
+			//this comes from the custom easing plugin
+			easing: 'easeInOutBack'
+		});
+	});
+
+	$(".previous").click(function(){
+		if(animating) return false;
+		animating = true;
+		
+		current_fs = $(this).parent();
+		previous_fs = $(this).parent().prev();
+		
+		//de-activate current step on progressbar
+		$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+		
+		//show the previous fieldset
+		previous_fs.show(); 
+		//hide the current fieldset with style
+		current_fs.animate({opacity: 0}, {
+			step: function(now, mx) {
+				//as the opacity of current_fs reduces to 0 - stored in "now"
+				//1. scale previous_fs from 80% to 100%
+				scale = 0.8 + (1 - now) * 0.2;
+				//2. take current_fs to the right(50%) - from 0%
+				left = ((1-now) * 50)+"%";
+				//3. increase opacity of previous_fs to 1 as it moves in
+				opacity = 1 - now;
+				current_fs.css({'left': left});
+				previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
+			}, 
+			duration: 800, 
+			complete: function(){
+				current_fs.hide();
+				animating = false;
+			}, 
+			//this comes from the custom easing plugin
+			easing: 'easeInOutBack'
+		});
+	});
 
 
-       
+
+	$scope.transf = function(user, dest, valor, contaUser, contaDest){
+		console.log(user);
+		console.log(dest);
+		console.log(valor);
+		console.log(contaUser);
+		console.log(contaDest);
+		
+		console.log(user);
+		console.log(dest);
+		if (user.titular == dest.titular) {
+			if (contaUser.tipo == contaDest.tipo) {
+				console.log("mesmo tipo de conta");
+			}
+			else{
+				console.log("transf de "+ contaUser.nome + " para " + contaDest.nome);
+				if (valor > user.saldo + user.limite) {
+					console.log("vc n tem grana");
+				}
+				else{
+					if (valor < user.saldo) {
+						console.log("vc vai usar seu saldo");
+					}
+					else{
+						console.log("vc vai usar do limite");
+					}
+				}
+			}
+
+		}
+		else{
+			if (valor > user.saldo + user.limite) {
+				console.log("vc n tem grana");
+			}
+			else{
+				if (valor < user.saldo) {
+					console.log("vc vai usar seu saldo");
+				}
+				else{
+					console.log("vc vai usar do limite");
+				}
+			}
+
+		}
+		
+	};
 
 
+    $scope.hasChanged = function() {
+		$("#agenciaDestino").val("");
+		$("#contaDestino").val("");
+	};
 });
 
