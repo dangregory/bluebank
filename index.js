a9f0d7550a7cbd7d3123ea15cd453520e317fbf8
Daniel Gregory <dangregory@hotmail.com>
2017-02-22 22:29:29 -0300

    Teste

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-var express = require('express');
+// web.js
+var gzippo = require('gzippo');
+var express = require("express");
+var logfmt = require("logfmt");
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.use(logfmt.requestLogger());
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.use(gzippo.staticGzip("" + __dirname + "/app"));
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+console.log("Listening on " + port);
 });
